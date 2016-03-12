2e513acab8b6baccd561535db1116680b861a122
Mihailo Puric <mpuric93@gmail.com>
2016-03-12 20:59:57 +0100

    Spaceship class added

---
+++ SpaceWars/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SpaceWars
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class EntryPoint
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new SpaceCraft())
+                game.Run();
+        }
+    }
+#endif
+}
---
+++ SpaceWars/Spaceship.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceWars
+{
+   public class Spaceship
+    {
+        public Vector2 Location;   
+        public Vector2 Velocity;    
+        private Texture2D image;
+        public Rectangle Bounds;
+        public int BulletCount;
+    }
+}
